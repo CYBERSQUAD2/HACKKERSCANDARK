5311cf627ef253d46d7a42745b7bae9595068deb
CYBERSQUAD2 <59162708+CYBERSQUAD2@users.noreply.github.com>
2019-12-23 15:25:06 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,49 @@
+
+
+clear
+cowsay -f eyes 'ARISANDI' | lolcat
+figlet -f slant "ARISANDI" | lolcat
+
+
+echo "Tools By Hrln.Arisandi" | lolcat
+echo "Author:ARISANDI" | lolcat
+echo "Team : HSD(HACKKER SCAN DARK)" | lolcat
+echo "====================" | lolcat
+
+echo "[~] Selamat mengggunakan Tool saya [~]"
+echo "[~] gunakan dengan bijak [~]"
+echo "[=] BY: :ARISANDI [=]"
+
+read -p "Masukkan Nomer Targer :" pilih
+read -p "Masukkan Jumlah Spam :" pilih
+
+
+case $pilih in
+*)
+echo "Nomer Target Di Temukan"
+sleep 3
+echo "Spam SMS Akan Di Mulai 3 Detik Lagi...."
+sleep 3
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SEND BERHASILL"
+sleep 1
+echo "SPAM SMS SUKSES"
+;;
+esac
